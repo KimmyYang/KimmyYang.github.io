bb5d1c3aca0d63cafcc593138334325f7955c237
KimmyYang <what_guitar@hotmail.com>
2016-10-10 00:08:58 +0800

    Add Project.html

--- js/selection_bar.js
+++ js/selection_bar.js
@@ -8,3 +8,6 @@
     function load_work(){
         document.getElementById("content").innerHTML='<object type="text/html" style="width: 100%;height: 100%;" data="sub_page/WorkDiary.html"></object>';
     }
+    function load_project(){
+        document.getElementById("content").innerHTML='<object type="text/html" style="width: 100%;height: 100%;" data="sub_page/Project.html"></object>';
+    }
