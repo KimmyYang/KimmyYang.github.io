7c0986cdaf8344ea58cf1901770c4e4eeea35f2b
KimmyYang <what_guitar@hotmail.com>
2016-10-09 23:11:52 +0800

    Add WorkDiary

---
+++ js/selection_bar.js
@@ -0,0 +1,10 @@
+    function load_home(){
+        document.getElementById("content").innerHTML='<object type="text/html" style="width: 100%;height: 100%;" data="sub_page/Home.html"></object>';
+    }
+    function load_intro(){
+            document.getElementById("content").innerHTML='<object type="text/html" style="width: 100%;height: 100%;" data="sub_page/Intro.html"></object>';
+    
+    }
+    function load_work(){
+        document.getElementById("content").innerHTML='<object type="text/html" style="width: 100%;height: 100%;" data="sub_page/WorkDiary.html"></object>';
+    }
